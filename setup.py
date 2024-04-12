ed5b83b76cf3226bced4806408657b4cf76cbb52
thivav <thivaharan28@gmail.com>
2024-04-12 23:00:42 +0530

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Object detection with Yolo-8',
+    author='thivav',
+    license='MIT',
+)
